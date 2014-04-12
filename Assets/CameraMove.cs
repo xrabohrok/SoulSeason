fcf192c896c184e6708492abd75033d8ee37feaa
Jaegersama <jaeger121us@gmail.com>
2014-04-12 13:40:17 -0500

    Rough draft level

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public float speed = 3.0f;
+	// Use this for initialization
+	void Update () 
+	{
+		//set movement
+		transform.position += transform.right*speed*Time.deltaTime;
+		
+	}
+
+}
