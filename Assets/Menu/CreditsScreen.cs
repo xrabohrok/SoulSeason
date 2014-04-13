ff448da690b42b2d0d8bf5079ceed3848b2f38ab
Westin Breger <westin007@gmail.com>
2014-04-12 20:30:07 -0500

    added player skipahead, organized menus - WKB

---
+++ Assets/Menu/CreditsScreen.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class CreditsScreen : MonoBehaviour 
+{
+    bool FirstLaunch = true;
+    bool pushedGoBack;
+
+    public Texture2D fadeTexture;
+    public Texture2D Credits;
+    public float buttonWidthPercent = .20f;
+    public float buttonHeightPercent = .05f;
+    public float fadeSpeed = .4f;
+    public float CreditsHeight = .03f;
+    public float CreditsScale = 1f;
+
+    private Rect TitlePos;
+    private float alpha = 1f;
+
+    public void OnGUI()
+    {
+        //Credtis
+        TitlePos.width = Credits.width * CreditsScale;
+        TitlePos.height = Credits.height * CreditsScale;
+        TitlePos.x = Screen.width / 2 - TitlePos.width / 2;
+        TitlePos.y = CreditsHeight;
+        GUI.DrawTexture(TitlePos, Credits);
+
+        float buttonWidth = Screen.width * buttonWidthPercent;
+        float buttonHeight = Screen.height * buttonHeightPercent;
+
+        if (FirstLaunch)
+        {
+            alpha = MenuFade.FadeIn(fadeSpeed, fadeTexture, alpha);
+            if (alpha <= 0)
+            {
+                FirstLaunch = false;
+            }
+        }
+
+        if(!FirstLaunch)
+        if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth/2, buttonHeight * 3, buttonWidth , buttonHeight), "Go Back"))
+        {
+            pushedGoBack = true;
+        }
+
+        if (pushedGoBack)
+        {
+            alpha = MenuFade.FadeOut(fadeSpeed, fadeTexture, alpha);
+            if(alpha >= 1)
+            {
+                Application.LoadLevel(0);
+            }
+        }
+    }
+}
+
---
+++ Assets/Menu/IntroMenuGUI.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMenuGUI : MonoBehaviour {
+
+    const int FIRST_LEVEL = 1;
+    bool pushedNewGame = false;
+    bool pushedQuit = false;
+    bool creditsPushed = false;
+
+    public Texture2D title;
+    public float buttonWidthPercent = .20f;
+    public float buttonHeightPercent = .05f;
+    public float spacing = 10f;
+    public float TitleHeight = .03f;
+    public float TitleScale = 1f;
+    public float fadeOutSpeed = 0.4f;
+    public Texture2D fadeTexture;
+
+    private Rect TitlePos;
+    private float alpha = 0.001f;
+
+    void OnGUI()
+    {
+        //Title
+        TitlePos.width = title.width * TitleScale;
+        TitlePos.height = title.height * TitleScale;
+        TitlePos.x = Screen.width/2 - TitlePos.width/2;
+        TitlePos.y = TitleHeight;
+        GUI.DrawTexture(TitlePos,title);
+
+        float buttonWidth = Screen.width * buttonWidthPercent;
+        float buttonHeight = Screen.height * buttonHeightPercent;
+
+        if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth/2, buttonHeight * 5, buttonWidth , buttonHeight), "New Game"))
+        {
+            if (NoOtherButtonsPushed())
+                pushedNewGame = true;
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, buttonHeight * 6 + 2 * spacing, buttonWidth, buttonHeight), "Credits"))
+        {
+            if (NoOtherButtonsPushed())
+                creditsPushed = true;
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, buttonHeight * 7 + 3 * spacing, buttonWidth, buttonHeight), "Quit"))
+        {
+            if (NoOtherButtonsPushed())
+                pushedQuit = true;
+        }
+
+        if (pushedNewGame)
+        {
+            alpha = MenuFade.FadeOut(fadeOutSpeed, fadeTexture, alpha);
+            if (alpha >= 1)
+                Application.LoadLevel(2);
+        }
+
+        if (creditsPushed)
+        {
+            alpha = MenuFade.FadeOut(fadeOutSpeed, fadeTexture, alpha);
+            if (alpha >= 1)
+                Application.LoadLevel(1);
+        }
+
+        if (pushedQuit)
+        {
+            Application.Quit();
+            Debug.Log("Quit");
+        }
+    }
+
+    bool NoOtherButtonsPushed()
+    {
+        return !pushedNewGame && !pushedQuit && !creditsPushed;
+    }
+
+
+
+
+
+}
